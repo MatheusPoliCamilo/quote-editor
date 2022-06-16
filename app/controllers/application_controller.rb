76e7aa18f3864617a024f2a414b6140c74376cba
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-15 23:51:44 -0300

    Change the progress bar style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # Uncomment this #before_action to see the progress bar long enough
+  # and remove it when it has the expected styles
+  # before_action -> { sleep 3 }
 end
