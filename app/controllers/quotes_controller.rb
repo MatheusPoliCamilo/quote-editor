6ff1f1f7f839633cfc3b9613df53971f0df7608c
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-15 23:07:29 -0300

    Send an unprocessable entity status if fails to update or create a quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,7 +18,7 @@ class QuotesController < ApplicationController
     if @quote.save
       redirect_to quotes_path, notice: "Quote was successfully created."
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -29,7 +29,7 @@ class QuotesController < ApplicationController
     if @quote.update(quote_params)
       redirect_to quotes_path, notice: "Quote was successfully updated."
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
