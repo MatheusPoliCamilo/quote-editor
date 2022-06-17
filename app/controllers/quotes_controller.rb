36d6e9f97e53ba2adcc3943534654239eae12042
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-16 23:21:20 -0300

    Change the way to delete the quotes using turbo stream

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -35,7 +35,11 @@ class QuotesController < ApplicationController
 
   def destroy
     @quote.destroy
-    redirect_to quotes_path, notice: "Quote was successfully destroyed."
+
+    respond_to do |format|
+      format.html { redirect_to quotes_path, notice: "Quote was successfully destroyed." }
+      format.turbo_stream
+    end
   end
 
   private
