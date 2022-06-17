118511f773319acdf9f7a86ac40c69af93abd092
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-16 23:09:15 -0300

    Work with turbo frames in the quotes listing

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -5,28 +5,6 @@ class QuotesTest < ApplicationSystemTestCase
     @quote = quotes(:first) # Reference to the first fixture quote
   end
 
-  test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
-
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
-    # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
-
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
-  end
-
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
@@ -34,14 +12,28 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: @quote.name
   end
 
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
