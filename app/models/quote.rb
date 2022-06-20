ae2c43baefdd246147d73a10231aacb79c67d65c
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 21:03:06 -0300

    Update the quotes list with websocket when create a quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,6 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
