4d6adb299920da81fa503e2e119b06b75dc86d58
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 22:10:33 -0300

    Update the quotes list with websocket when update or delete a quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,4 +4,6 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   after_create_commit -> { broadcast_prepend_to "quotes" }
+  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
