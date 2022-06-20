e2841ee9829f30d05d897c6e34906bf5c154abc6
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 22:39:18 -0300

    Add company entity to the application

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_014557) do
+ActiveRecord::Schema.define(version: 2022_06_20_013738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
