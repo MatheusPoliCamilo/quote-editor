9fb54e34ad64d4256510729dfa51cf12a4289e47
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 22:16:21 -0300

    Change the quotes broadcast to work asynchronous using background jobs

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,7 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes" }
   after_destroy_commit -> { broadcast_remove_to "quotes" }
 end
