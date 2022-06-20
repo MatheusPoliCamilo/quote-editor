42661f5f9c4c57d1fb4215b59ee5b5d4a2527cd7
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 22:18:50 -0300

    Refactor the Quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,9 @@ class Quote < ApplicationRecord
 
   scope :ordered, -> { order(id: :desc) }
 
-  after_create_commit -> { broadcast_prepend_later_to "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes" }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
