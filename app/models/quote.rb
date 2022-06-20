db290d826ba12dd7ee1d17e10c10200fd7e13a81
Matheus Poli <matheuspolicamilo@gmail.com>
2022-06-19 23:24:42 -0300

    Fix the security issue with the quote broadcast, scoping by company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -7,7 +7,7 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
   belongs_to :company
 end
